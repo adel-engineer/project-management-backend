447257b73cf62aad6a40d567e32fa93795867eb2
unknown <adel.s.engineer@gmail.com>
2026-07-21 13:34:24 +0530

    add project models

---
+++ src/models/project.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const projectSchema = new Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        description:{
+            type: String
+        },
+        createdBy:{
+            type: Schema.Types.objectId,
+            ref: "User",
+            required: true
+        }
+
+    }, { timestamps: true }
+);
+
+
+const Project = mongoose.model("Project", projectSchema)
