7a259429a307ba696f72e8ef9cdc26fb59d7b90e
galal <ad127el@gmail.com>
2026-07-26 16:56:57 +0530

    Add project permission middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model.js");
 const ApiError = require("../utils/api-error.js");
 const asyncHandler = require("../utils/asyncHandler.js");
+const ProjectMember = require("../models/projectmember.model.js");
+const { default: mongoose } = require("mongoose");
 
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
@@ -46,4 +48,36 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
         );
     }
 });
+
+const validateProjectPermission = (roles = []) =>{
+    asyncHandler(async (req, res, next)=>{
+        const {projectId} = req.params;
+
+        if(!projectId){
+            throw new ApiError(400, "Project id is missing")
+        }
+
+       const project = await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id)
+        })
+
+        if(!project){
+            throw new ApiError(400, "Project not found")
+        }
+
+        const givenRole = project?.role
+
+        req.user.role = givenRole
+        
+        if(!roles.includes(givenRole)){
+            throw new ApiError(
+                403,
+                "You do not have permission to perform this action"
+            )
+        }
+
+        next()
+    });
+}
 module.exports = verifyJWT
