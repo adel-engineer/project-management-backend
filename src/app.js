03dd94d2f46c8e76d61bb8bbdd65ad77efdda0aa
galal <ad127el@gmail.com>
2026-08-06 22:57:00 +0530

    feat: add Helmet middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require("cors");   
 const app = express();
 const cookieParser = require('cookie-parser');
+const helmet = require("helmet");
+
 
 //middleware
 app.use(
@@ -16,6 +18,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use(helmet());
 
 
 
